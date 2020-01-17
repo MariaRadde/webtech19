50daf1357e7083f4c1403af3d1174132db281939
MariaRadde <56865389+MariaRadde@users.noreply.github.com>
2020-01-17 19:57:40 +0100

    Update uebung6.php

--- Aufgabe 6/uebung6.php
+++ Aufgabe 6/uebung6.php
@@ -1,1 +1,45 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
+    <title>Aufgabe 6</title>
+    <?php
+    require_once "./mockdataarray.php";
+    ?>
+</head>
+<body>
 <?php
+echo "<h3> Tabelle</h3>";
+echo '<table class="table table-striped table-hover table-responsive">
+<thead> <tr>';
+
+for($i=0; $i<count($player[0]); $i++) //nur die Obere Name der spalten
+{
+    echo '<th>'.$player[0][$i].'</th>';
+}
+echo '</tr> </thead> 
+<tbody>';
+for($i=1; $i<count($player); $i++) //0-50 zeilen
+{
+    echo '<tr>';
+    for($j=0; $j<count($player[$i]); $j++) //0-3 spalten(inhalt)
+    {
+        if ($j!=3 )
+        {
+            echo '<td>'.$player[$i][$j].'</td>';
+        }
+        else
+        {
+            echo '<td><a href="forward.php?id='.$i.'">'.$player[$i][$j].'</a></td>'; //get-methode manuell erstellen
+        }
+
+    }
+    echo '</tr>';
+}
+echo '</tbody> </table>';
+?>
+</body>
+</html>
+
