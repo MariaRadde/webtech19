56c637b945a146fa4012883925e24ca77e424b26
MariaRadde <56865389+MariaRadde@users.noreply.github.com>
2020-01-17 20:44:23 +0100

    Update script.php

--- lesson2/script.php
+++ lesson2/script.php
@@ -1,1 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
 <?php
+$name=$_GET["name"];
+$nachname=$_GET["nachname"];
+$email=$_GET["email"];
+echo $_SERVER["PHP_SELF"];
+?>
+<ul>
+    <li>name=<?php   echo $name; ?> </li>
+    <li>nachname=<?php   echo $nachname; ?> </li>
+    <li>email=<?php   echo $email; ?> </li>
+</ul>
+</body>
+</html>
+
