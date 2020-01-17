73a7c8ded3d3f7acf38da49395ea98716c4c0500
MariaRadde <56865389+MariaRadde@users.noreply.github.com>
2020-01-17 20:43:17 +0100

    Update index.php

--- KlausurVorbr/index.php
+++ KlausurVorbr/index.php
@@ -1,1 +1,36 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
+    <title>Test</title>
+    <style>
+        body{
+            font-family:Verdana,sans-serif;
+        }
+        header {background-color: lightblue;
+        color:darkgray;
+            width: 100%;
+            text-align: center;
+        }
+        footer{
+            position: :absolute;
+            bottom: 0px;
+            background-color: #0c5460;
+            text-align: center;
+
+        }
+        footer>a, footer>a:hover,footer>a:link
+    </style>
+</head>
+<body class="container-fluid">
+
+<footer>
+
+    <a href="webtech.f4.htw-berlin.de/"
+</footer>
+</body>
+
+</html>
+
